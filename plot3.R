473e342b7ea342d1876f2ba789755bbfd39c6b93
Dekuran <declan.clowry@gmail.com>
2016-01-24 19:25:30 +0000

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -4,6 +4,6 @@ lines(pow2$DateTime,pow2$Sub_metering_2,type="l",col="red")
 lines(pow2$DateTime,pow2$Sub_metering_3, type="l", col = "blue")
 legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
        col=c("black","blue","red"), lty=1, cex=.7)
-
+## export to png file
 dev.copy(png,'plot3.png',width=480,height=480,units="px")
 dev.off()
